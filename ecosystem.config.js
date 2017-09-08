124a07ed0859185760bff265c721d4684e88c533
zhaofinger <zhaohaibo1994@gmail.com>
2017-09-08 12:07:55 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -35,7 +35,7 @@ module.exports = {
 			ref: 'origin/master',
 			repo: 'git@github.com:zhaofinger/finger-blog.git',
 			path: '/var/www/finger-blog',
-			'post-deploy': 'yarn && pm2 reload ecosystem.config.js --env production'
+			'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
 		}
 	}
 };
