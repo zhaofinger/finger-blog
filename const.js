acd8172e234f4417e3ce2f72264047e7ba6e90df
zhaofinger <zhaohaibo1994@gmail.com>
2018-12-14 17:05:55 +0800

    [fix] 图片前缀修改

--- const.js
+++ const.js
@@ -5,6 +5,6 @@ const IMG_TYPE = {
   ARTICLE_COVER: 3,
   FILM: 4
 };
-const IMG_PRE = env === 'production' ? 'http://ow3w4ozuv.bkt.clouddn.com/' : 'http://owu5r8hjs.bkt.clouddn.com/';
+const IMG_PRE = env === 'production' ? 'http://image.finger.asimr.top/' : 'http://owu5r8hjs.bkt.clouddn.com/';
 
 module.exports = { IMG_TYPE, IMG_PRE };
